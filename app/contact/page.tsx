990a670f463cccf9d89835fde73083880ff6f673
Kevin <uzamurerakevin01@gmail.com>
2025-03-17 12:17:05 +0200

    add all  updates

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,17 +1,15 @@
-"use client";
+"use client"; 
 
-import ContactTop from "@/components/contact/ContactTop"; 
-
-import React from 'react';
-import ContactUsPage from '@/components/contact/ContactUs';
-import Link from 'next/link'; // Import the Link component
+import React from "react";
+import ContactTop from "@/components/contact/Contact";
+import ContactUsPage from "@/components/contact/ContactUs";
+import Link from "next/link"; 
 
 const Page = () => {
-
   return (
-    <main className="">
-     < ContactTop />
-     <ContactUsPage />
+    <main>
+      <ContactTop /> 
+      <ContactUsPage />
     </main>
   );
 };
